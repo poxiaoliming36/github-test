d58285166c628ed06135c4f5d3c1ce7d0db560cb
liqiang <1546315068@qq.com>
2020-09-16 06:40:12 +0800

    demo-mapper第二次提交，添加了StudentController

---
+++ demo-mapper/src/main/java/com/example/demomapper/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.example.demomapper.controller;
+
+/**
+ * @Description TODO
+ * @Date 2020/9/16 0:13
+ * @Author by liqiang
+ */
+public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("lili hello");
+    }
+}
